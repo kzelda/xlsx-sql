5db4b95d6ce8b3e81ee7e5f7d863b08066e4de32
kzelda <kzeldan64@gmail.com>
2019-10-16 11:24:19 +0100

    apply trim on values

--- src/examples/export_sql/app.js
+++ src/examples/export_sql/app.js
@@ -1,3 +1,3 @@
 var lib = require("../../lib");
 
-lib.Sql(`${__dirname}/file2.xlsx` , `${__dirname}/Script_${Math.random()}.sql`);
+lib.Sql(`${__dirname}/file4.xlsx` , `${__dirname}/Script.sql`);
--- src/lib/index.js
+++ src/lib/index.js
@@ -7,13 +7,18 @@ Object.defineProperty(exports, "__esModule", {
 var xlsx = require('node-xlsx').default;
 var fs = require("fs");
 
-//var fs = require("fs");
 
 var Data = function Data(xlsx_path) {
     return xlsx.parse(xlsx_path);
 };
 
-var Sql = function Sql(xlsx_file,sql_file) {
+/**
+ * 
+ * @param {string} xlsx_file Xlsx file path
+ * @param {string} sql_file Sql script path
+ * @param {string} sql_dialect sqlserver | mysql | postgres
+ */
+var Sql = function Sql(xlsx_file, sql_file, sql_dialect) {
 
     var xlsx = Data(xlsx_file);
 
@@ -33,63 +38,60 @@ GO
 CREATE TABLE [${tablename}](
 `;
 
-    for (let i = 0; i < header.length; i++) {
-        sheader += `  [${header[i]}] nvarchar(max)${i == header.length - 1 ? "\r\n)" : ",\r\n"}`;
+    for (var i = 0; i < header.length; i++) {
+        var header_i = header[i];
+
+        if (typeof header_i === "undefined") {
+            header_i = `Col${header_i}`;
+        }
+
+        sheader += `  [${header_i}] nvarchar(max)${i == header.length - 1 ? "\r\n)" : ",\r\n"}`;
     }
 
     //var sql_file = `${__dirname}/Script_${Math.random()}.sql`;
 
-    async function append_file(s) {
-        await fs.appendFileSync(sql_file, s, null);
+    function append_file(s) {
+        (async function () {
+            await fs.appendFileSync(sql_file, s, null);
+        }());
     }
 
     append_file(`${sheader}\r\n\r\n`);
 
-    for (let i = 1; i < data.length; i++) {
+    for (var i = 1; i < data.length; i++) {
 
         var data_i = data[i];
 
         // fill empty with undefined to use map
-        for (let j = 0; j < data_i.length; j++) {
-            if (!data_i[j]) {
+        for (var j = 0; j < data_i.length; j++) {
+            if (typeof data_i[j] === "undefined") {
                 data_i[j] = undefined;
             }
         }
 
         // add string delimiters
-        var q1 = data_i.map(function (x, idx) {
+        var q_values = data_i.map(function (x, idx) {
 
-            if (x == null || x == undefined) {
-                return 'null';
-            }
+                if (x == null || x == undefined) {
+                    return 'null';
+                }
 
-            return "'" + x.toString()
-                .replace(/\r/g, "")
-                .replace(/\n/g, "")
-                .replace(/'/g, "''") + "'";
+                return "'" + x.toString()
+                    .replace(/(\r\n|\r|\n)/g, "")
+                    .replace(/'/g, "''")
+                    .trim() + "'";
 
-        });
+            })
+            .join(",");
 
         // export sql insert
-        (async function () {
-            await append_file(`INSERT INTO ${tablename} VALUES(${q1.join(",")})\r\n`);
-        })();
+        append_file(`INSERT INTO ${tablename} VALUES(${q_values})\r\n`);
     }
+
+    append_file("\r\n".repeat(3) + `-- Count =  ${data.length -1}`);
+
+    append_file(`\r\nSELECT COUNT(*) FROM ${tablename}`);
 }
 
-// var ImportJsonFromFile = function Json(xlsx_path) {
-//     return JSON.stringify(Data(xlsx_path));
-// };
-
-// var ExportJsonToFile = function WriteFile(xlsx_path,json_path) {
-
-//     fs.writeFile(json_path,
-//         ImportJsonFromFile(xlsx_path),
-//         function(x){}
-//     );
-// };
-
-// exports.Json = ImportJsonFromFile;
-// exports.WriteFile = ExportJsonToFile;
 exports.Data = Data;
 exports.Sql = Sql;
